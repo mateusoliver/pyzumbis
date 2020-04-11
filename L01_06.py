a7dde1fd1282748fa631761569806e9eec936845
Mateus Oliveira <mateusoliveirathe@gmail.com>
2020-04-11 18:41:48 -0300

    Exercício 05 da lista 01 - calculando o tempo de uma viagem.

---
+++ L01_06.py
@@ -0,0 +1,4 @@
+distancia = float(input('Entre com a distancia em m. Ex: 3.5,4.2,5.7,6.9: '))
+velocidade = int(input('entre com q velocidade m´edia, ex: 40,50 :'))
+media = distancia/velocidade
+print (f'O tempo que a viagem ir´a durar ser´a de: {media} horas')
