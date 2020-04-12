e73ce62a64f6116d25fffcaac55a066ea743e7e6
Mateus Oliveira <mateusoliveirathe@gmail.com>
2020-04-12 11:46:52 -0300

    Exercício 09 da lista 01 - Cálculo aluguel de carro.

---
+++ L01_09.py
@@ -0,0 +1,7 @@
+kmpercorrido = int(input('quantos km fotam percorridos: '))
+diasalugados = int(input('Quantos dias o carro foi alugado: '))
+valorkm = kmpercorrido*0.15
+valordia = diasalugados*60
+total = valorkm+valordia
+
+print (total)
